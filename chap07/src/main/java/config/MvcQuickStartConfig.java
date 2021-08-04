4218e62e9cd5f616cea8ebc40173d2bd31763fbf
seungmin-park <tmddudals369@naver.com>
2021-08-04 23:40:48 +0900

    servlet config xml → Configuration

---
+++ chap07/src/main/java/config/MvcQuickStartConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import quickstart.SecondHelloController;
+
+@Configuration
+@EnableWebMvc
+public class MvcQuickStartConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public SecondHelloController secondhelloController(){
+        return new SecondHelloController();
+    }
+}
